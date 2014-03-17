267947d662e9e34e14225346575a18cf2ec79f94
fsiola <fsiola@gmail.com>
2014-03-16 22:19:45 -0300

    ronse and barat hmt

--- HMTGrayScalePy/HMTGrayScalePy/HMT.py
+++ HMTGrayScalePy/HMTGrayScalePy/HMT.py
@@ -4,13 +4,23 @@ from ia636 import *
 from ia870 import *
 
 #Khosravi and Schafer Hit-Or-Miss Transformation
-def ksHMT(f, b):
-    erosionFB = iaero(f, b)
-    fMinus = ianeg(f)
-    bMinus = ianeg(b)
-    erosionFMinusBMinus = iaero(fMinus, bMinus)
-    result = erosionFB + erosionFMinusBMinus
-    return result
+def ksHMT(f, bfg):
+    if len(f.shape) == 1:
+        f = expand_dims(f,0)
+
+    if len(bfg.shape) == 1:
+        bfg = expand_dims(bfg,0)
+        
+    eroFBfg = iaero(f, bfg)
+    eroMFMBfg = iaero(ianeg(f), ianeg(bfg))
+
+    resultImage = zeros(f.shape)
+
+    for x in range(f.shape[0]):
+        for y in range(f.shape[1]):
+            resultImage[x][y] = eroFBfg[x][y] - eroMFMBfg[x][y]
+
+    return resultImage
 
 #Soille Unconstrained Hit-Or-Miss Transformation
 def suHMT(f,bfg, bbg):
@@ -27,6 +37,8 @@ def suHMT(f,bfg, bbg):
     if len(bbg.shape) == 1:
         bbg = expand_dims(bbg,0)
 
+    bbg = ianeg(bbg)
+
     eroFBfg = iaero(f, bfg)
     dilBbg = iadil(f, bbg)
 
@@ -34,10 +46,62 @@ def suHMT(f,bfg, bbg):
 
     for x in range(f.shape[0]):
         for y in range(f.shape[1]):
-            if(dilFBbg[x][y] <= eroFBfg[x][y]):
-                resultImage[x][y] = ero[x][y] - dilFBbg[x][y]
+            resultImage[x][y] = max(eroFBfg[x][y] - dilFBbg[x][y], 0)
 
     return resultImage
+
+#Barat Hit-or-Miss Transformation
+def bHMT(f, bfg, bbg):
+    if len(f.shape) == 1:
+        f = expand_dims(f,0)
+
+    if len(bfg.shape) == 1:
+        bfg = expand_dims(bfg,0)
+
+    if len(bbg.shape) == 1:
+        bbg = expand_dims(bbg,0)
+
+    bbg = ianeg(bbg)
+
+    eroFBfg = iaero(f, bfg)
+    dilBbg = iadil(f, bbg)
+
+    resultImage = zeros(f.shape)
+
+    for x in range(f.shape[0]):
+        for y in range(f.shape[1]):
+            resultImage[x][y] = dilFBbg[x][y] - eroFBfg[x][y]
+
+    return resultImage    
+
+#Ronse Hit-Or-Miss Transformation
+def rHMT(f, bfg, bbg):
+    if len(f.shape) == 1:
+        f = expand_dims(f,0)
+
+    if len(bfg.shape) == 1:
+        bfg = expand_dims(bfg,0)
+
+    if len(bbg.shape) == 1:
+        bbg = expand_dims(bbg,0)
+
+    bbg = ianeg(bbg)
+
+    eroFBfg = iaero(f, bfg)
+    dilBbg = iadil(f, bbg)
+
+    maxValue = max(eroFBfg, max(dilBbg)) + 1
+
+    resultImage = zeros(f.shape)
+
+    for x in range(f.shape[0]):
+        for y in range(f.shape[1]):
+            if(ero[x][y] >= dilBbg[x][y] != maxvalue):
+                resultImage[x][y] = eroFBfg[x][y]
+            else:
+                resultImage[x][y] = maxValue
+
+    return resultImage    
 
 #Raducana and Grana Hit-Or-Miss Transformation
 def rgHMT(f, bfg, bbg):
@@ -77,5 +141,4 @@ def rgHMT(f, bfg, bbg):
 
         #not sure here
         resultImage += tempBinHMT
-
     return resultImage
